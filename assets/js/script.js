91f575fdda2d2376ce01d1d4be878eb99b1b2e51
coding.tarun <coding.tarun@gmail.com>
2025-01-28 10:47:53 +0530

    Initial Setup

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * JavaScript Key notes :
+ * 1. A dynamic , weakly types programming language whcih is compalied at runtime.
+ * 2. Aimed to make a webpage more dynamic and interactive.
+ *
+ * Ex. JS enables us to make a new request to the server without the need to reloading the entire webpage.
+ * 
+ *
+ *
+ */
